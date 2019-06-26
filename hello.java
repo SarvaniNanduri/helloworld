fa8e80d91aa2b55dadb44c332fbdcecb1041e691
Prashant kulkarni <prashant.kulkarni@aspiresys.com>
2019-06-26 13:00:49 +0530

    changes mase to check the build

--- hello.java
+++ hello.java
@@ -5,7 +5,10 @@ public class hello
 	for(int i=0;i<5;i++)
 	{
 	  System.out.println("Hello, Welcome to Java Project ----" +i);
+	  
 	}
+	
+	System.out.println("----This is to test the build ---");
     
   }
 }
