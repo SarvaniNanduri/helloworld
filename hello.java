323a7e01b615390cd8a5397914678b48d12d563c
sarvani nanduri <sarvani.nanduri@aspiresys.com>
2019-07-05 17:03:52 +0530

    to test the build for new branch(dev1) after specifying the build trigger module

--- hello.java
+++ hello.java
@@ -10,6 +10,7 @@ public class hello
 	
 	System.out.println("----This is to test the build ---");
 	System.out.println("----This is to test the build for dev1 branch----");
+	System.out.println("This is to test the build for dev1 branch");
     
   }
 }
